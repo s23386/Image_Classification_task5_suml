cc76096d7902975923ec29ef3ffb681bd245a6c4
s23386 <133228375+s23386@users.noreply.github.com>
2024-03-09 22:26:32 +0100

    Create classifier.py

---
+++ classifier.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from fastai.vision.all import *
+from fastai.vision.widgets import *
+
+# Function to load the model
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return load_learner("https://github.com/s23386/Image_Classification_task5_suml/raw/main/s23386_model_image.pkl")
+
+# Define Streamlit app
+def main():
+    # Load the model
+    model = load_model()
+    
+    # Set title and description
+    st.title("Dog or Cat Classifier")
+    st.write("Upload an image and we'll classify it as either a dog or a cat.")
+    
+    # Upload image
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    
+    if uploaded_file is not None:
+        # Display the uploaded image
+        img = PILImage.create(uploaded_file)
+        st.image(img, caption='Uploaded Image', use_column_width=True)
+        
+        # Make prediction
+        pred, pred_idx, probs = model.predict(img)
+        
+        # Display the prediction
+        st.write(f"Prediction: {pred}; Probability: {probs[pred_idx]:.4f}")
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    main()
