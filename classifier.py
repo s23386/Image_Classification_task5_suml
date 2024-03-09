179391c02237d0f7f6dc6414815ebc91439b98d8
s23386 <133228375+s23386@users.noreply.github.com>
2024-03-09 22:49:50 +0100

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,12 +1,10 @@
 import streamlit as st
 from fastai.vision.all import *
-from fastai.vision.widgets import *
 
 # Function to load the model
 @st.cache(allow_output_mutation=True)
 def load_model():
     return load_learner("s23386_image_model_NEW.pkl")
-    #https://github.com/s23386/Image_Classification_task5_suml/blob/main/s23386_model_image.pkl
 
 # Define Streamlit app
 def main():
